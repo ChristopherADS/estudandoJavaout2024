143562d2d1fb5269490eedfdd30fd133ad52581e
Christopher <christopher.contato@gmail.com>
2024-10-22 18:29:40 -0300

    to aqui estudando 22.10

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+
+    String Snopse;
+    Snopse = """
+É nois
+Taca marcha
+negocio enrolado
+Desenrola ai
+""";
+    //System.out.println(Snopse);
+
+        String senha = "123456";
+        if (senha.equals("12345")) {
+            System.out.println("Acesso autorizado!");
+        } else {
+            System.out.println("Senha incorreta.");
+        }
+
+    }
+}
+
+
---
+++ src/soma.java
@@ -0,0 +1,6 @@
+public class soma {
+    public static void main(String[] mdsPqArgs) {
+        System.out.println(10+5);
+    }
+}
+
